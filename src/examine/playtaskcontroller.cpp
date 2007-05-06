63ac8000dd4220b4cb08c5b8de0209f6651abcc9
unknown <devnull@localhost>
2007-05-06 10:28:51 +0000

    skip empty entries during play

--- src/examine/playtaskcontroller.cpp
+++ src/examine/playtaskcontroller.cpp
@@ -32,11 +32,25 @@ PlayTaskController::~PlayTaskController()
 
 bool PlayTaskController::hasNext()
 {
-    return d->index < model->rowCount();
+    int nextIndex = d->index;
+    while ( nextIndex < model->rowCount() ) {
+        QModelIndex i = model->index(nextIndex, StudyTaskModel::QuestionColumn);
+        QString question = model->data(i).toString();
+        if ( !question.isEmpty() ) break;
+        nextIndex++;
+    }
+    return nextIndex < model->rowCount();
 }
 
 ControllerDataEntry PlayTaskController::next()
 {
+    while ( d->index < model->rowCount() ) {
+        QModelIndex i = model->index(d->index, StudyTaskModel::QuestionColumn);
+        QString question = model->data(i).toString();
+        if ( !question.isEmpty() ) break;
+        d->index++;
+    }
+
     ControllerDataEntry entry;
 
     entry.answer = model->data( model->index(d->index, StudyTaskModel::AnswerColumn) ).toString();
