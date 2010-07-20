75da10cc9e15c52d71d7c8ff1ce0e0d1f8f96dbc
yshurik <devnull@localhost>
2010-07-20 20:43:21 +0000

    Initial commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  EnBasic
+//
+//  Created by Oleksandr Iakovliev on 7/17/10.
+//  Copyright LynxLine 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
